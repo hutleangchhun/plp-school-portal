de7b7d5985284210edc878c85f56a5a4fb0c2dab
hutleangchhun <hutleangchhun@gmail.com>
2025-09-17 17:25:28 +0700

    implement disable console on production

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import removeConsole from 'vite-plugin-remove-console'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeConsole()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
